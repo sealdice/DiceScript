70555bf73cd3316a1b286f366fd83fe2c5c28847
fy0 <fy0@users.noreply.github.com>
2022-11-29 14:24:31 +0000

    deploy: ef64c390c02260693d12fa15e3cf32bc97ec73e3

--- main.go
+++ main.go
@@ -5,20 +5,37 @@ package main
 
 import (
 	"github.com/gopherjs/gopherjs/js"
-	"github.com/sealdice/dicescript"
+	dice "github.com/sealdice/dicescript"
+	"regexp"
+	"strconv"
 )
 
-var scope = map[string]*dicescript.VMValue{}
+var scope = map[string]*dice.VMValue{}
 
 func newVM(name string) *js.Object {
-	player := dicescript.VMValueNewDict(nil)
+	player := dice.VMValueNewDict(nil)
+	player.Store("力量", dice.VMValueNewInt(50))
+	player.Store("敏捷", dice.VMValueNewInt(60))
+	player.Store("智力", dice.VMValueNewInt(70))
 	scope["player"] = player.V()
 
-	vm := dicescript.NewVM()
-	vm.ValueStoreFunc = func(name string, v *dicescript.VMValue) {
-		scope[name] = v
-	}
-	vm.ValueLoadFunc = func(name string) *dicescript.VMValue {
+	vm := dice.NewVM()
+	//vm.ValueStoreFunc = func(name string, v *dice.VMValue) {
+	//	scope[name] = v
+	//}
+
+	re := regexp.MustCompile(`^_(\D+)(\d+)$`)
+	vm.ValueLoadFunc = func(name string) *dice.VMValue {
+		m := re.FindStringSubmatch(name)
+		if len(m) > 1 {
+			val, _ := strconv.ParseInt(m[2], 10, 64)
+			return dice.VMValueNewInt(val)
+		}
+
+		if v, exists := player.Load(name); exists {
+			return v
+		}
+
 		if val, ok := scope[name]; ok {
 			return val
 		}
@@ -29,24 +46,25 @@ func newVM(name string) *js.Object {
 }
 
 func main() {
-	newDict := func() *dicescript.VMDictValue {
-		return dicescript.VMValueNewDict(nil)
+	newDict := func() *dice.VMDictValue {
+		return dice.VMValueNewDict(nil)
 	}
 
-	newValueMap := func() *dicescript.ValueMap {
-		return &dicescript.ValueMap{}
+	newValueMap := func() *dice.ValueMap {
+		return &dice.ValueMap{}
 	}
 
 	js.Global.Set("dice", map[string]interface{}{
 		"newVM":        newVM,
 		"newValueMap":  newValueMap,
-		"vmNewInt64":   js.MakeWrapper(dicescript.VMValueNewInt),
-		"vmNewFloat64": js.MakeWrapper(dicescript.VMValueNewFloat),
-		"vmNewStr":     js.MakeWrapper(dicescript.VMValueNewStr),
-		"vmNewDict":    js.MakeWrapper(newDict),
-		"help":         js.MakeWrapper("此项目的js绑定: https://github.com/sealdice/dicescript"),
+		"vmNewInt64":   js.MakeWrapper(dice.VMValueNewInt),
+		"vmNewFloat64": js.MakeWrapper(dice.VMValueNewFloat),
+		"vmNewStr":     js.MakeWrapper(dice.VMValueNewStr),
+		//"vmNewArray":    js.MakeWrapper(newArray),
+		"vmNewDict": js.MakeWrapper(newDict),
+		"help":      js.MakeWrapper("此项目的js绑定: https://github.com/sealdice/dice"),
 	})
 
-	//js.Module.Set("newVM", dicescript.NewVM)
-	//js.Module.Set("Context", dicescript.Context{})
+	//js.Module.Set("newVM", dice.NewVM)
+	//js.Module.Set("Context", dice.Context{})
 }
